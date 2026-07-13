31174a2e0c559a1e19667565558f577b7e62462e
Sneha Sharma <snehasharmaa912@gmail.com>
2026-07-13 13:52:09 +0530

    feat: replay event log on startup and wire live event log into server

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"payflow"
 )
 
 func main() {
-	store := payflow.NewStore()
-	api := payflow.NewAPI(store)
+	logPath := os.Getenv("PAYFLOW_LOG_PATH")
+	if logPath == "" {
+		logPath = "payflow.log"
+	}
 
-	log.Println("payflow listening on :8080")
+	store, err := payflow.ReplayFromLog(logPath)
+	if err != nil {
+		log.Fatalf("replaying event log: %v", err)
+	}
+
+	eventLog, err := payflow.OpenEventLog(logPath)
+	if err != nil {
+		log.Fatalf("opening event log: %v", err)
+	}
+	defer eventLog.Close()
+	store.SetEventLog(eventLog)
+
+	api := payflow.NewAPI(store).WithLogPath(logPath)
+
+	log.Printf("payflow listening on :8080 (event log: %s)", logPath)
 	if err := http.ListenAndServe(":8080", api.Routes()); err != nil {
 		log.Fatal(err)
 	}
