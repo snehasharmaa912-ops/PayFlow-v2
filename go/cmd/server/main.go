88945a5aa7b1a73a5eac04ece037dc8a4cd4f6fa
Sneha Sharma <snehasharmaa912@gmail.com>
2026-07-13 14:11:00 +0530

    feat: wire risk evaluator into server via PAYFLOW_RISK_JAR env var

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -26,6 +26,13 @@ func main() {
 	defer eventLog.Close()
 	store.SetEventLog(eventLog)
 
+	if jarPath := os.Getenv("PAYFLOW_RISK_JAR"); jarPath != "" {
+		store.SetRiskEvaluator(payflow.NewProcessRiskEvaluator(jarPath))
+		log.Printf("risk engine enabled: %s", jarPath)
+	} else {
+		log.Println("PAYFLOW_RISK_JAR not set; large charges will skip the risk check")
+	}
+
 	api := payflow.NewAPI(store).WithLogPath(logPath)
 
 	log.Printf("payflow listening on :8080 (event log: %s)", logPath)
